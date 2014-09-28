64691f1e97b98a6e5842d57ceaf522dfa4652955
galahade <galahade@sina.com>
2014-09-28 23:32:13 +0800

    add mvc support

---
+++ src/main/java/com/salmon/scommerce/config/WebAppInitializer.java
@@ -0,0 +1,34 @@
+package com.salmon.scommerce.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return new Class<?>[]{MybatisConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+	
+	@Override
+	  protected Filter[] getServletFilters() {
+		CharacterEncodingFilter charcterEncodingFilter = new CharacterEncodingFilter();
+		charcterEncodingFilter.setEncoding("UTF-8");
+		return new Filter[] { charcterEncodingFilter};
+	}
+
+}
---
+++ src/main/java/com/salmon/scommerce/web/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.salmon.scommerce.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
+	
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String getHomePage() {
+		LOG.debug("Home page will be display.");
+		ModelAndView view = new ModelAndView();
+		view.setViewName("home");
+		return "home";
+	}
+}
