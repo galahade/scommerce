99a330fd6b9ce05ea6e23f4445e2883e034089bd
galahade <galahade@sina.com>
2014-09-28 17:18:31 +0800

    add transaction manager

--- src/main/java/com/salmon/scommerce/config/DataSourceConfig.java
+++ src/main/java/com/salmon/scommerce/config/DataSourceConfig.java
@@ -4,11 +4,11 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
@@ -32,5 +32,10 @@ public class DataSourceConfig {
 		dataSource.setPassword(evn.getProperty("mysql.password"));
 		
 		return dataSource;
+	}
+	
+	@Bean
+	public DataSourceTransactionManager txManager() {
+		return new DataSourceTransactionManager(dataSource());
 	}
 }
