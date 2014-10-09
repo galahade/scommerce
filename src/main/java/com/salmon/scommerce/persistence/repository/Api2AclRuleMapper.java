7d9f8826a3a4d4fc8bb26a82e28b520041bf204c
ctian <ctian@salmon.com>
2014-10-09 18:22:07 +0800

    add update and delete in AclRule.
    Change-Id: Ib2f09f4e7423afad91c84b999c91fae63adf257f

--- src/main/java/com/salmon/scommerce/persistence/repository/Api2AclRuleMapper.java
+++ src/main/java/com/salmon/scommerce/persistence/repository/Api2AclRuleMapper.java
@@ -2,7 +2,11 @@ package com.salmon.scommerce.persistence.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.salmon.scommerce.persistence.domain.Api2AclRule;
 
@@ -14,10 +18,21 @@ import com.salmon.scommerce.persistence.domain.Api2AclRule;
  */
 public interface Api2AclRuleMapper {
 	
+	@Select("SELECT * FROM api2_acl_rule where entity_id = #{entityId}")
+	@ResultMap("Api2AclRuleResult")
+	public Api2AclRule getAclRuleByEntityId(int entityId);
+	
 	public List<Api2AclRule> getAclRules(Api2AclRule api2AclRule);
 	
 	@Insert("INSERT INTO api2_acl_rule(role_id, resource_id, privilege ) VALUES(#{roleId}, #{resourceId}, #{privilege})")
 	public int addAclRule(Api2AclRule api2AclRule);
+	
+	@Update("UPDATE api2_acl_rule SET role_id = #{roleId},resource_id = #{resourceId},privilege = #{privilege} "
+			+ "where entity_id = #{entityId} ")
+	public void updateRuleByEntityId(Api2AclRule api2AclRule);
+	
+	@Delete("DELETE FROM api2_acl_rule WHERE entity_id = #{entityId}")
+	public void deleteByEntityId(int entityId);
 
 	
 
